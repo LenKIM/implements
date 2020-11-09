36e9e05d8ba463d90e23125c106b6aff9cfa580e
lenkim <joenggyu0@gmail.com>
2020-11-09 16:23:29 +0900

    두수의 합 풀이 추가

---
+++ Len/chapter06/p2.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/reverse-string/
+from typing import List
+
+
+class Solution:
+  def reverseString(self, s: List[str]) -> None:
+# 투 포인터를 이용한 스왑
+
+    left, right = 0, len(s) - 1
+    while left < right:
+      s[left], s[right] = s[right], s[left]
+      left += 1
+      right -= 1
+
+# 파이썬다운 방식
+    s.reverse()
