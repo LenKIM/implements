b06d2e8322908dfcd0d1877940481e1ea44b2e79
lenkim <joenggyu0@gmail.com>
2020-11-17 06:06:29 +0900

    가장 긴 팰린드롬 추가

---
+++ Len/chapter06/p6-longest-palindrome-substring.py
@@ -0,0 +1,25 @@
+# 가장 긴 팰린드론 부분 문자열을 출력하기
+# babad > bab
+
+class Solution:
+
+    def longestPalindrome(self, s: str) -> str:
+        def expand(left: int, right: int) -> str:
+            # 팰린드롬 판별 및 투 포인터 확장
+            while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
+                left -= 1
+                right += 1
+            return s[left + 1: right - 1]
+
+        # 해당 사항이 없을 때 빠르게 리턴
+        if len(s) < 2 or s == s[::-1]:
+            return s
+
+        result = ''
+        # 슬라이딩 원도우 우측으로 이동
+        for i in range(len(s) - 1):
+            result = max(result,
+                         expand(i, i + 1),
+                         expand(i, i + 2),
+                         key=len)
+        return result
