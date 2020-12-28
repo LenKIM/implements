20332af6ad9e98aad7033e184e2d80e02e8c6ac9
lenkim <joenggyu0@gmail.com>
2020-12-28 16:12:12 +0900

    modify package and solve-p22

--- Len/ch06/p3.py
+++ Len/ch06/p3.py
@@ -21,18 +21,19 @@ from typing import List
 
 
 class Solution(object):
-  def reorderLogFiles(self, logs):
-    """
-    :type logs: List[str]
-    :rtype: List[str]
-    """
-    letters = []
-    nums = []
-    for log in logs:
-      logsplit = log.split(" ")
-      if logsplit[1].isalpha():
-        letters.append((" ".join(logsplit[1:]), logsplit[0]))
-      else:
-        nums.append(log)
-    letters.sort()
-    return [letter[1] + " " + letter[0] for letter in letters] + nums
+
+    def reorderLogFiles(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: List[str]
+        """
+        letters = []
+        nums = []
+        for log in logs:
+            logsplit = log.split(" ")
+            if logsplit[1].isalpha():
+                letters.append((" ".join(logsplit[1:]), logsplit[0]))
+            else:
+                nums.append(log)
+        letters.sort()
+        return [letter[1] + " " + letter[0] for letter in letters] + nums
