b92fc42c217f46757ae548ab46274c2df2622272
lenkim <joenggyu0@gmail.com>
2021-01-09 08:30:13 +0900

    add p32-p33 problems

---
+++ Len/ch12-graph/p32-number-of-islands.py
@@ -0,0 +1,36 @@
+# 1을 육지, 0을 물로 가정한 2D 그래프 맴에서, 섬의 개수를 구하라
+from typing import List
+
+
+class Solution:
+
+    def dfs(self, grid: List[List[str]], i, j):
+        # 더 이상 땅이 아닌 경우 종료
+        if i < 0 or i >= len(grid) or \
+                j < 0 or j >= len(grid[0]) or \
+                grid[i][j] != '1':
+            return
+
+        grid[i][j] = 0
+
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    # 모든 육지 탐색 후 카운트 1증가
+                    count += 1
+        return count
+
+
+solution = Solution()
+grid = [["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]
+
+solution.numIslands(grid)
