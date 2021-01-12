bc4177170f487e3d024662115317edd0765aad64
lenkim <joenggyu0@gmail.com>
2021-01-12 10:09:31 +0900

    add p36-p37 problems

--- Len/ch12-graph/p34-permutations.py
+++ Len/ch12-graph/p34-permutations.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+    # nPn
     def permute(self, nums: List[int]) -> List[List[int]]:
         results = []
         prev_elements = []
@@ -9,20 +10,27 @@ class Solution:
         def dfs(elements):
             # 리프 노드일 때 결과 추가
             if len(elements) == 0:
-                elements_ = prev_elements[:]
+                elements_ = prev_elements[:] # Stack
                 results.append(elements_)
 
             # 순열 생성 재귀 호출
-            for e in elements:
-                next_elements = elements[:]
-                next_elements.remove(e)
+            for e in elements: # 순서대로
+                temp = elements[:]
+                temp.remove(e)
 
+# 위치를 찾아가서 지워
+# temp - [1,2,3] | [2,3] | [3]   |  []  result.append    |  [3]       | [2,3] | [2]
+# prev = []      | [1]   | [1,2] |  [1,2,3]              | popup [1,2]| [1]   | [1,3]
                 prev_elements.append(e)
-                dfs(next_elements)
-
+                dfs(temp)
                 prev_elements.pop()
 
-        dfs(nums)
+
+
+
+
+
+
         return results
 
 
