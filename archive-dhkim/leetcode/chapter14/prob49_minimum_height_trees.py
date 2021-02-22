6e951cb6cfcaf45f91375cb9073b807c4b2e73f3
dhkdn9192 <dhkdn9192@naver.com>
2021-02-22 13:22:26 +0900

    ADD LEETCODE_minimum-height-trees, UPDATE HACKERRANK_castle-on-the-grid

---
+++ archive-dhkim/leetcode/chapter14/prob49_minimum_height_trees.py
@@ -0,0 +1,52 @@
+from typing import List
+from collections import defaultdict
+
+"""
+https://leetcode.com/problems/minimum-height-trees
+"""
+
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+
+        if not edges:
+            return [0]
+
+        vertex2neighbors = defaultdict(list)
+        for vertexes in edges:
+            vertex2neighbors[vertexes[0]].append(vertexes[1])
+            vertex2neighbors[vertexes[1]].append(vertexes[0])
+
+        memoization = defaultdict(dict)
+
+        def get_max_depth(now_v, pre_v):
+            neighbors = [v for v in vertex2neighbors[now_v] if v != pre_v]
+            if not neighbors:
+                return 0
+
+            depth_list = []
+            for nb in neighbors:
+                if nb in memoization[now_v]:
+                    depth = memoization[now_v][nb]
+                else:
+                    depth = get_max_depth(nb, now_v) + 1
+                    memoization[now_v][nb] = depth
+
+                depth_list.append(depth)
+
+            return max(depth_list)
+
+        vertex2depths = defaultdict(list)
+        for v in vertex2neighbors.keys():
+            d = get_max_depth(v, -1)
+            vertex2depths[d].append(v)
+
+        # print(vertex2depths)
+
+        return sorted(vertex2depths.items(), key=lambda t: t[0])[0][1]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    res = solution.findMinHeightTrees(6, [[3,0],[3,1],[3,2],[3,4],[5,4]])
+    print(f">> res : {res}")
