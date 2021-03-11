086a9543668fceb7ee5175fe4784b770e4f1fe0f
jaewon.lim <jewon945@gmail.com>
2021-03-12 00:07:28 +0900

    ADD hackerrank bubble_sort, mark_and_toys, comparator

---
+++ archive-JWL/sort/comparator.py
@@ -0,0 +1,27 @@
+from functools import cmp_to_key
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def __repr__(self):
+        return self
+    def comparator(a, b):
+        temp = b.score -a.score
+        if temp == 0:
+            if a.name > b.name:
+                temp = 1
+            else:
+                temp = -1
+        return temp
+
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
