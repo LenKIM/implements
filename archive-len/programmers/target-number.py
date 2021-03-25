35774364bd2c2f152bece35074b1ed3a40e592bf
lenkim <joenggyu0@gmail.com>
2021-03-26 02:09:39 +0900

    ADD target-number

---
+++ archive-len/programmers/target-number.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43165
+from typing import List
+
+
+def solution(numbers: List, target: int):
+    answer = 0
+    return answer
+
+
+i = solution([1, 1, 1, 1, 1], 3)
+print(i)
