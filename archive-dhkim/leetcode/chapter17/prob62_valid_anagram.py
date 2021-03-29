add8bc5c30235552be702f8e6bbb25444d9e4dbc
dhkdn9192 <dhkdn9192@naver.com>
2021-03-29 12:10:20 +0900

    ADD LEETCODE_62_valid-anagram

---
+++ archive-dhkim/leetcode/chapter17/prob62_valid_anagram.py
@@ -0,0 +1,10 @@
+"""
+https://leetcode.com/problems/valid-anagram
+"""
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(s) == sorted(t):
+            return True
+        return False
