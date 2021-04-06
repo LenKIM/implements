51430d0bcbda4106f9c283e4ed0dea2749f5c37d
lenkim <joenggyu0@gmail.com>
2021-04-06 09:43:02 +0900

    MODIFY: LEETCODE-p63

--- archive-len/leetcode/ch18-binary-search/p63-binary-search.py
+++ archive-len/leetcode/ch18-binary-search/p63-binary-search.py
@@ -3,22 +3,25 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
         def dfs(_nums, _target, low=None, high=None):
-            low, high = low or 0, high or len(nums) - 1
+            low = low or 0
+            if high == None:
+                high = len(_nums) - 1
 
             if low > high:
                 return -1
             mid = (low + high) // 2
 
-            if nums[mid] > target:
-                return dfs(nums, target, low, mid)
-            if nums[mid] == target:
+            if _nums[mid] > target:
+                return dfs(_nums, target, low, mid - 1)
+            if _nums[mid] == target:
                 return mid
-            if nums[mid] < target:
-                return dfs(nums, target, mid + 1, high)
+            if _nums[mid] < target:
+                return dfs(_nums, target, mid + 1, high)
 
         return dfs(nums, target)
 
 
-search = Solution().search([-1, 0, 3, 5, 9, 12], 9)
+search = Solution().search([-1,0,3,5,9,12], 9)
 print(search)
