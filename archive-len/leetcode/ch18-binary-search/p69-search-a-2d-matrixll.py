c14e2d1ab5b67736688b59ff764182820f034875
lenkim <joenggyu0@gmail.com>
2021-04-13 05:51:54 +0900

    ADD: LEETCODE_p69 search-a-2d-matrix-ii

---
+++ archive-len/leetcode/ch18-binary-search/p69-search-a-2d-matrixll.py
@@ -0,0 +1,17 @@
+from typing import List
+
+'''
+
+https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+'''
+
+
+class Solution:
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # return any(target in x for x in matrix)
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        return False
+
+
