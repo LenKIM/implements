2286f9d34351ee01272126b1a04b720eab925b2f
jaewon.lim <jewon945@gmail.com>
2021-04-13 10:17:00 +0900

    ADD leetcode prob69_search_a_2d_matrix.py

---
+++ archive-JWL/leetCode/Chapter17/prob70_single_number.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res = res ^ i
+        return res
