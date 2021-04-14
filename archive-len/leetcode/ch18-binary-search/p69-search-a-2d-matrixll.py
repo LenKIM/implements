fff576ed060fb8da6fa5e1271b9e041533c08614
lenkim <joenggyu0@gmail.com>
2021-04-14 09:43:24 +0900

    ADD: p70-single-number

--- archive-len/leetcode/ch18-binary-search/p69-search-a-2d-matrixll.py
+++ archive-len/leetcode/ch18-binary-search/p69-search-a-2d-matrixll.py
@@ -12,6 +12,18 @@ class Solution:
         # return any(target in x for x in matrix)
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+
+        row, col = 0, len(matrix[0]) - 1
+
+        while row <= len(matrix) - 1 and col >= 0:
+
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col = col - 1
+            elif matrix[row][col] < target:
+                row = row + 1
+
         return False
-
-
