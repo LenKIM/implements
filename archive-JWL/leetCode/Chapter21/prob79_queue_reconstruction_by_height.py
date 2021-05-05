179758aec4771c724f82cf7665530c5fce8ff5bb
jaewon.lim <jewon945@gmail.com>
2021-05-05 18:10:09 +0900

    ADD prob79_queue_reconstruction_by_height.py

---
+++ archive-JWL/leetCode/Chapter21/prob79_queue_reconstruction_by_height.py
@@ -0,0 +1,13 @@
+import heapq
+from typing import List
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        heap = []
+        for person in people:
+            heapq.heappush(heap,(-person[0],person[1]))
+        result=[]
+        while heap:
+            person = heapq.heappop(heap)
+            result.insert(person[1], [-person[0],person[1]])
+        return result
